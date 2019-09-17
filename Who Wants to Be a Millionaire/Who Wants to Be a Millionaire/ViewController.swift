55e0dc9541f241915a5f996b1c2008c88173341b
Sasha <sanderok12@gmail.com>
2019-09-17 20:44:11 +0300

    lab1

---
+++ Who Wants to Be a Millionaire/Who Wants to Be a Millionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Who Wants to Be a Millionaire
+//
+//  Created by Tytorenko Sasha on 9/17/19.
+//  Copyright © 2019 sanderok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
